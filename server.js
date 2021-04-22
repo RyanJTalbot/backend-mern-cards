db544e7d4c5ec5b352e19e7d97350604b9b997fe
Ryan JT <ryantalbot7@gmail.com>
2021-04-21 19:45:27 -0500

    Update server

--- server.js
+++ server.js
@@ -125,10 +125,9 @@ require('./routes/authRoutes')(app);
 // 	);
 // }
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-app.get('*', (req, res) =>
-	res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
-);
+// ex. template express.static(root, [options])
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
